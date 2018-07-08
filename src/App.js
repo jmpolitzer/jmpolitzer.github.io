7959fecd0da15eb431c377247834fc03bbb55fb0
jmpolitzer <jmpolitzer@gmail.com>
2018-07-08 16:53:45 -0500

    adds react-live

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import LiveEdit from './components/LiveEdit';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <LiveEdit code='<strong>Hello World</strong>'/>
+      </div>
+    );
+  }
+}
+
+export default App;
