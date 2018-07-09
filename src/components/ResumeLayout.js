b0926654e8f7df172010f072be2545e086affb1c
jmpolitzer <jmpolitzer@gmail.com>
2018-07-09 17:22:33 -0500

    renders main body of resume

--- src/components/ResumeLayout.js
+++ src/components/ResumeLayout.js
@@ -1,6 +1,7 @@
 const ResumeLayout = (`
   const LayoutWrapper = styled.div\`
     display: block;
+    padding: 25px;
     color: rgb(141, 141, 141);
   \`;
 
@@ -12,6 +13,7 @@ const ResumeLayout = (`
   const Header = styled.h4\`
     margin-left: 10px;
     font-weight: bold;
+    color: rgb(85, 140, 171);
   \`;
 
   const Body = styled.div\`
@@ -23,7 +25,6 @@ const ResumeLayout = (`
   const SideBar = styled.div\`
     width: 25%;
     padding: 0px 10px 10px;
-    word-wrap: break-word;
   \`;
 
   const SideBarGroup = styled.div\`
@@ -38,18 +39,32 @@ const ResumeLayout = (`
 
   const Main = styled.div\`
     width: 75%;
-    height: 200px;
-    background-color: green;
+    padding: 0px 10px 10px;
+  \`;
+
+  const MainSection = styled.div\`
+    margin-bottom: 10px;
+  \`;
+
+  const ExperienceList = styled.ul\`
+    margin-top: 0px;
+    padding-left: 17px;
   \`;
 
   const BoldText = styled.div\`
-    font-weight: bold;
+    font-weight: 600;
     font-size: 10px;
     margin-bottom: 2px;
   \`;
 
+  const LongText = styled.div\`
+    font-weight: 300;
+    font-size: 10px;
+    line-height: 13px;
+  \`;
+
   const TitleColor = {
-    color: 'rgb(85, 140, 171)'
+    color: 'rgb(113, 159, 185)'
   };
 
   const data = JSON.parse(content);
@@ -57,7 +72,7 @@ const ResumeLayout = (`
   render(
     <LayoutWrapper>
       <HorizontalRule />
-      <Header style={TitleColor}>{data.name.toUpperCase()}</Header>
+      <Header>{data.name.toUpperCase()}</Header>
       <Body>
         <SideBar>
           <SideBarGroup>
@@ -90,7 +105,33 @@ const ResumeLayout = (`
             {data.references.length === 0 && <BoldText>Available upon request.</BoldText>}
           </SideBarGroup>
         </SideBar>
-        <Main />
+        <Main>
+          <MainSection>
+            <BoldText style={TitleColor}>Profile</BoldText>
+            <LongText>{data.profile}</LongText>
+          </MainSection>
+          <MainSection>
+            <BoldText style={TitleColor}>Experience</BoldText>
+            {data.experience.map((exp, i) => {
+              return <div>
+                      <BoldText>{exp.title}, {exp.company}; {exp.location} &ndash; {exp.duration}</BoldText>
+                      <ExperienceList>
+                        {exp.highlights.map((hl, j) => {
+                          return <li><LongText>{hl}</LongText></li>;
+                        })}
+                      </ExperienceList>
+                    </div>;
+                  })}
+          </MainSection>
+          <MainSection>
+            <BoldText style={TitleColor}>Education</BoldText>
+            {data.education.map((edu, i) => {
+              const degree = edu.degree ? edu.degree + ',' : '';
+
+              return <BoldText>{edu.school} &ndash; {degree} {edu.year}</BoldText>;
+            })}
+          </MainSection>
+        </Main>
       </Body>
     </LayoutWrapper>
   );
