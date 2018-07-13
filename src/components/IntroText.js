42753f25a4c993034ce623247be7f144233beb94
jmpolitzer <jmpolitzer@gmail.com>
2018-07-13 15:00:47 -0500

    adds small text changes and gitignore headshot

--- src/components/IntroText.js
+++ src/components/IntroText.js
@@ -21,7 +21,7 @@ const IntroText = ({ showResume }) => (
     <Typist.Delay ms={1500} />
     <br key={2} />
     <br key={3} />
-    <SmallText>In a few seconds, my resume and the React code that renders it will appear.</SmallText>
+    <SmallText>In a few moments, my r&eacute;sum&eacute; and the React code that renders it will appear.</SmallText>
     <Typist.Delay ms={1000} />
     <br key={4} />
     <SmallText>Take a gander at all three flavors and print to your heart's desire.</SmallText>
